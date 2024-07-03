6cea06f15cf1cba0998df58bc40b173247e1622d
BalavikneshRam-Codes <balaram0711@gmail.com>
2024-07-03 10:44:39 +0530

    Spotify-Clone Version - 1

---
+++ src/assets/assets.js
@@ -0,0 +1,221 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.jpeg'
+import img2 from './img2.jpeg'
+import img3 from './img3.jpeg'
+import img4 from './img4.jpeg'
+import img5 from './img5.jpeg'
+import img6 from './img6.jpeg'
+import img7 from './img7.jpeg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
+import img11 from './img11.jpg'
+import img12 from './img12.jpg'
+import img13 from './img13.jpg'
+import img14 from './img14.jpg'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+import song4 from './song4.mp3'
+import song5 from './song5.mp3'
+import song6 from './song6.mp3'
+import song7 from './song7.mp3'
+import song8 from './song8.mp3'
+import song9 from './song9.mp3'
+import song10 from './song10.mp3'
+import song11 from './song11.mp3'
+
+import img81 from './img81.jpeg'
+import img91 from './img91.jpeg'
+import img101 from './img101.jpeg'
+import img111 from './img111.jpeg'
+
+
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Global",
+        image: img8,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "Top 50 India",
+        image: img9,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#22543d"
+    },
+    {   
+        id:2,
+        name: "Trending India",
+        image: img10,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#742a2a"
+    },
+    {   
+        id:3,
+        name: "Trending Global",
+        image: img16,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#44337a"
+    },
+    {   
+        id:4,
+        name: "Mega Hits,",
+        image: img11,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "Happy Favorites",
+        image: img15,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Diamonds",
+        image: img1,
+        file:song1,
+        desc:"Rihanna",
+        duration:"4:42"
+    },
+    {
+        id:1,
+        name: "Daylight",
+        image: img2,
+        file:song2,
+        desc:"David Kushner",
+        duration:"3:49"
+    },
+    {
+        id:2,
+        name: "I Was Never There",
+        image: img3,
+        file:song3,
+        desc:"The Weekend Gesaffeistein",
+        duration:"4:01"
+    },
+    {
+        id:3,
+        name: "Living Life, In The Night",
+        image: img4,
+        file:song4,
+        desc:"Cheriimoya Sierra Kidd",
+        duration:"2:18"
+    },
+    {
+        id:4,
+        name: "En Jannal Vandha",
+        image: img5,
+        file:song5,
+        desc:"Yuvan Shankar Raja,Roshini,Priya Hemesh,Divya Vijay",
+        duration:"4:26"
+    },
+    {
+        id:5,
+        name: "Night Changes",
+        image: img6    ,
+        file:song6,
+        desc:"One Direction",
+        duration:"4:00"
+    },
+    {
+        id:6,
+        name: "Middle of the Night",
+        image: img7,
+        file:song7,
+        desc:"Elley Duhe",
+        duration:"3:10"
+    },
+    {
+        id:7,
+        name: "Illuminati - Aavesham",
+        image: img81,
+        file:song8,
+        desc:"Sushin Shyam,Debzee,Vinayak Sasikumar",
+        duration:"2:28"
+    },
+    {
+        id:8,
+        name: "Stereo Hearts ",
+        image: img91,
+        file:song9,
+        desc:"Gym Class Heroes,Adam Levine",
+        duration:"3:36"
+    },
+    {
+        id:9,
+        name: "7 years",
+        image: img101,
+        file:song10,
+        desc:"Lukas Graham",
+        duration:"3:53"
+    },
+    {
+        id:10,
+        name: "Summertime Sadness",
+        image: img111,
+        file:song11,
+        desc:"Lana Del Rey",
+        duration:"4:25"
+    }
+]
